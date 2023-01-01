8f43b023284721ec6e9626aaab90f867a1c1b24f
levitran <tranthanhvan1590@gmail.com>
2023-01-01 15:39:34 +0700

    ok

--- VPaged.Example/VPaged.Example/Connection.cs
+++ VPaged.Example/VPaged.Example/Connection.cs
@@ -2,6 +2,6 @@
 {
     public static class Connection
     {
-        public const string ConnectionString = "Data Source=DESKTOP-K919664\\LEVITRAN;Initial Catalog=VPagedEx;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public const string ConnectionString = "Data Source=DESKTOP-K919664\\LEVITRAN;Initial Catalog=VPagedExample;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     }
 }
