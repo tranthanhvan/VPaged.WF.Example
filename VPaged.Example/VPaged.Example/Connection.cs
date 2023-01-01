ad10303121c84b4f0fdfb3e6ae39f8931a31394e
levitran <tranthanhvan1590@gmail.com>
2023-01-01 15:41:49 +0700

    clear

--- VPaged.Example/VPaged.Example/Connection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace VPaged.Example
-{
-    public static class Connection
-    {
-        public const string ConnectionString = "Data Source=DESKTOP-K919664\\LEVITRAN;Initial Catalog=VPagedExample;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-    }
-}
