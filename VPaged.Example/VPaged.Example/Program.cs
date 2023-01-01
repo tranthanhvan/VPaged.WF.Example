35b060fffcf837a78336ef67a201cc9578aef180
levitran <tranthanhvan1590@gmail.com>
2023-01-01 15:27:17 +0700

    resolve lib

--- VPaged.Example/VPaged.Example/Program.cs
+++ VPaged.Example/VPaged.Example/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VPaged.Example
@@ -16,7 +13,7 @@ namespace VPaged.Example
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmEmployee());
         }
     }
 }
